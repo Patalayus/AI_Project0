1e85678c4622b59d0be72c39ba8c0c4be92129a6
Luke Charles <lukecharles222@gmail.com>
2017-10-11 21:09:30 +0100

    inheritance over MainSentient

--- src/MainSentient.java
+++ src/MainSentient.java
@@ -3,7 +3,8 @@ import sun.applet.Main;
 import java.util.Scanner;
 public class MainSentient {
     //global variables
-    String SubName;
+    public double count = 0;
+    public double SubName0;
 
     public static void main(String args[]) {
         //this is my AI project (0.0.1)
@@ -13,7 +14,7 @@ public class MainSentient {
         String resp;
         String resp1;
         String resp2;
-        double count = 0;
+        double config;
 
         //initialise input
         Scanner enps = new Scanner(System.in);
@@ -23,6 +24,7 @@ public class MainSentient {
         MainSentient stay = new MainSentient();
 
         System.out.println("Hi, I'm Alex");
+        config=1;
         resp = enps.nextLine();
         if ((resp.equals("hi alex")) || (resp.equals("Hi Alex")
                 || (resp.equals("hello alex") || (resp.equals("Hello Alex") || (resp.equals("Hi alex") || (resp.equals("hi Alex")
@@ -33,37 +35,48 @@ public class MainSentient {
             if ((resp1.equals("yes")) || (resp1.equals("Yes") || (resp1.equals("Yeah") || (resp1.equals("yeah") || (resp1.equals("Sure") || (resp1.equals("sure") || (resp1.equals("Okay") || (resp1.equals("okay"))))))))) {
                 System.out.println("What is your name?");
                 SubName = enps.nextLine();
+                //parse into global equivalent
+
                 if ((SubName != null)) {
                     System.out.println("Well, hello " + SubName);
                     stay.nextf();
                 }
+            }else{
+                //option no
+                jump.val_option();
             }
         } else {
-            if (count == 0) {
-                System.out.printf(jump.out0());
-                resp2 = enps.nextLine();
-                if ((resp2.equals("hi alex")) || (resp2.equals("Hi Alex")
-                        || (resp2.equals("hello alex") || (resp2.equals("Hello Alex") || (resp2.equals("Hi alex") || (resp.equals("hi Alex")
-                        || (resp2.equals("Hi Alex") || (resp2.equals("Hi Alex") || (resp2.equals("Hi") || (resp2.equals("hi") || (resp2.equals("Hello") || (resp2.equals("hello")
-                )))))))))))) {
-                    System.out.println("Can I ask you a question?");
-                    resp1 = enps.nextLine();
-                    if ((resp1.equals("yes")) || (resp1.equals("Yes") || (resp1.equals("Yeah") || (resp1.equals("yeah") || (resp1.equals("Sure") || (resp1.equals("sure") || (resp1.equals("Okay") || (resp1.equals("okay"))))))))) {
-                        System.out.println("What is your name?");
-                        SubName = enps.nextLine();
-                        if ((SubName != null)) {
-                            System.out.println("Well, hello " + SubName);
-                            stay.nextf();
+            double count = 0;
+                if(count==0) {
+                    System.out.printf(jump.out0());
+                    resp2 = enps.nextLine();
+                    if ((resp2.equals("hi alex")) || (resp2.equals("Hi Alex")
+                            || (resp2.equals("hello alex") || (resp2.equals("Hello Alex") || (resp2.equals("Hi alex") || (resp.equals("hi Alex")
+                            || (resp2.equals("Hi Alex") || (resp2.equals("Hi Alex") || (resp2.equals("Hi") || (resp2.equals("hi") || (resp2.equals("Hello") || (resp2.equals("hello")
+                    )))))))))))) {
+                        System.out.println("Can I ask you a question?");
+                        resp1 = enps.nextLine();
+                        if ((resp1.equals("yes")) || (resp1.equals("Yes") || (resp1.equals("Yeah") || (resp1.equals("yeah") || (resp1.equals("Sure") || (resp1.equals("sure") || (resp1.equals("Okay") || (resp1.equals("okay"))))))))) {
+                            System.out.println("What is your name?");
+                            SubName = enps.nextLine();
+                            //parse into global equivalent
+
+                            if ((SubName != null)) {
+                                System.out.println("Well, hello " + SubName);
+                                stay.nextf();
+                            }
+                        } else {
+                            //option no
+                            jump.val_option();
                         }
                     }
                 }
             }
-        }
     }public void nextf(){
         //local declaration of jump function (specific to this subroutine)
-        SecondSentient jump_local0 = new SecondSentient();
+        SecondSentient jump_0 = new SecondSentient();
 
-        System.out.println("do you know what my favourite colour is "+SubName+"?");
-        jump_local0.colourpreferance();
+        System.out.println("What's your favourite colour?");
+        jump_0.colourpreferance();
     }
 }
--- src/SecondSentient.java
+++ src/SecondSentient.java
@@ -1,9 +1,22 @@
-public class SecondSentient {
+import java.util.Scanner;
+public class SecondSentient extends MainSentient {
+    public double check;
+
     public String out0(){
         return String.format("Are you going to say hi?");
-
     }
     public void colourpreferance(){
+        System.out.println("GOT TO COLOUR");
+        System.out.println(count);
+        Scanner and = new Scanner(System.in);
 
     }
+    public void val_option(){
+
+    }
+    public String check_run() {
+        check = 1;
+        return String.format(""+check);
+    }
+
 }
