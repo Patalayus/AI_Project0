557cda34339608b3e55e5e9d180500167cdaa192
Luke Charles <Chillichickenwings@googlemail.com>
2017-10-12 09:57:31 +0100

    grammar update

--- src/MainSentient.java
+++ src/MainSentient.java
@@ -27,18 +27,25 @@ public class MainSentient {
         config=1;
         resp = enps.nextLine();
         if ((resp.equals("hi alex")) || (resp.equals("Hi Alex")
-                || (resp.equals("hello alex") || (resp.equals("Hello Alex") || (resp.equals("Hi alex") || (resp.equals("hi Alex")
-                || (resp.equals("Hi Alex") || (resp.equals("Hi Alex") || (resp.equals("Hi") || (resp.equals("hi") || (resp.equals("Hello") || (resp.equals("hello")
-        )))))))))))) {
+                || (resp.equals("hello Alex")|| (resp.equals("Hello alex")
+                || (resp.equals("hello alex") || (resp.equals("Hello Alex")
+                || (resp.equals("Hi alex") || (resp.equals("hi Alex")
+                || (resp.equals("Hi Alex") || (resp.equals("Hi Alex")
+                || (resp.equals("Hi") || (resp.equals("hi")
+                || (resp.equals("Hello") || (resp.equals("hello")
+        )))))))))))))) {
             System.out.println("Can I ask you a question?");
             resp1 = enps.nextLine();
-            if ((resp1.equals("yes")) || (resp1.equals("Yes") || (resp1.equals("Yeah") || (resp1.equals("yeah") || (resp1.equals("Sure") || (resp1.equals("sure") || (resp1.equals("Okay") || (resp1.equals("okay"))))))))) {
+            if ((resp1.equals("yes")) || (resp1.equals("Yes") || (resp1.equals("Yeah")
+                    || (resp1.equals("yeah") || (resp1.equals("Sure")
+                    || (resp1.equals("sure") || (resp1.equals("Okay") || (resp1.equals("okay")
+                    ||(resp1.equals("yea")||(resp1.equals("Yea")||(resp1.equals("YEA")))))))))))) {
                 System.out.println("What is your name?");
                 SubName = enps.nextLine();
                 //parse into global equivalent
 
                 if ((SubName != null)) {
-                    System.out.println("Well, hello " + SubName);
+                    System.out.println("Well, hello " + SubName+".");
                     stay.nextf();
                 }
             }else{
@@ -51,18 +58,25 @@ public class MainSentient {
                     System.out.printf(jump.out0());
                     resp2 = enps.nextLine();
                     if ((resp2.equals("hi alex")) || (resp2.equals("Hi Alex")
-                            || (resp2.equals("hello alex") || (resp2.equals("Hello Alex") || (resp2.equals("Hi alex") || (resp.equals("hi Alex")
-                            || (resp2.equals("Hi Alex") || (resp2.equals("Hi Alex") || (resp2.equals("Hi") || (resp2.equals("hi") || (resp2.equals("Hello") || (resp2.equals("hello")
+                            || (resp2.equals("hello alex") || (resp2.equals("Hello Alex")
+                            || (resp2.equals("Hi alex") || (resp.equals("hi Alex")
+                            || (resp2.equals("Hi Alex") || (resp2.equals("Hi Alex")
+                            || (resp2.equals("Hi") || (resp2.equals("hi") || (resp2.equals("Hello")
+                            || (resp2.equals("hello")
                     )))))))))))) {
                         System.out.println("Can I ask you a question?");
                         resp1 = enps.nextLine();
-                        if ((resp1.equals("yes")) || (resp1.equals("Yes") || (resp1.equals("Yeah") || (resp1.equals("yeah") || (resp1.equals("Sure") || (resp1.equals("sure") || (resp1.equals("Okay") || (resp1.equals("okay"))))))))) {
+                        if ((resp1.equals("yes")) || (resp1.equals("Yes") || (resp1.equals("Yeah")
+                                || (resp1.equals("yeah") || (resp1.equals("Sure")
+                                || (resp1.equals("sure") || (resp1.equals("Okay")
+                                || (resp1.equals("okay")||(resp1.equals("yea")
+                                ||(resp1.equals("Yea")||(resp1.equals("YEA")))))))))))) {
                             System.out.println("What is your name?");
                             SubName = enps.nextLine();
                             //parse into global equivalent
 
                             if ((SubName != null)) {
-                                System.out.println("Well, hello " + SubName);
+                                System.out.println("Well, hello " + SubName+".");
                                 stay.nextf();
                             }
                         } else {
--- src/SecondSentient.java
+++ src/SecondSentient.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 public class SecondSentient extends MainSentient {
     public double check;
 
-    public String out0(){
+    public String out0() {
         return String.format("Are you going to say hi?");
     }
-    public void colourpreferance(){
+
+    public void colourpreferance() {
         Scanner and = new Scanner(System.in);
 
         //local variables
-        String colourpref =and.nextLine();
-        if((colourpref.equals("purple"))||(colourpref.equals("Purple")||(colourpref.equals("PURPLE")))){
-        System.out.println("Hey! My favourite colour is purple too!");
-        }else{
-        System.out.println("I'm not really a fan of "+colourpref+".\nI think " +
-                "my favourite colour is purple.");
-        System.out.println("So, ");
+        String colourpref = and.nextLine();
+        if ((colourpref.equals("purple")) || (colourpref.equals("Purple") || (colourpref.equals("PURPLE")))) {
+            System.out.println("Hey! My favourite colour is purple too!");
+        } else {
+            System.out.println("I'm not really a fan of " + colourpref + ".\nI think " +
+                    "my favourite colour is purple.");
         }
-    }
+        System.out.println("I don't really know what to say now.");
+}
     public void val_option(){
         System.out.println("Well I guess this is it then...");
     }
